bc6f09717c4dfe1776ea7596e38e063a55a46e23
ron34353435 <ron34353435@gmail.com>
2024-08-07 22:59:44 +0300

    fix settings

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "CahsCam"
+    PROJECT_NAME: str = "CashCam"
     PROJECT_VERSION: str = "1.0.0"
     OBJECT_DETECTION_MODEL: str = "models/bill_coin_yolo_best.pt"
     CLASSIFICATION_MODEL: str = "models/class_YOLO_model_best.pt"
-    IMAGE_BASE_URL: str = "https://www.example.com/images" #TODO
     API_PREFIX: str = "/api"
     TEST_OUTPUT_PATH: str = "tests/test_output.txt"
-    PORT: int = 80
+    PORT: int = 8000
     LOCAL_IP: str = "0.0.0.0"
     DEBUG: bool = True
 
-    class ConfigDict:
+    class Config:
         env_file = ".env"
 
 settings = Settings()
